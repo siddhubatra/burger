f106a5c926d4f923afc708e4fe51fa53c4015cc8
thesidbatra <thesidbatra@gmail.com>
2019-08-05 11:59:27 -0400

    finalizing

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,15 @@
-// Set up MySQL connection.
 var mysql = require("mysql");
+var keys = require('./keys');
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db"
-});
+var connection = mysql.createConnection(keys.mysql);
 
 // Make connection.
-connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
 
-// Export connection for our ORM to use.
 module.exports = connection;
---
+++ config/dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+    mysql: {
+        user: "root",
+        password: "password",
+        port: 3306,
+        database: 'burgers_db',
+        host: 'localhost'
+    }
+};
